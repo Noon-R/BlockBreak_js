edaeb558076554640e0c0d90275215f0c8b82874
Noon-R <r1234psp@yahoo.co.jp>
2020-05-06 23:43:35 +0900

    Set Coolision

--- js/game.js
+++ js/game.js
@@ -3,14 +3,18 @@ var ctx = canvas.getContext("2d");
 
 var bx = canvas.width/2;
 var by = canvas.height/2;
+var br = 10;
 var dx = 1;
 var dy = 1;
 
-var wx = [0];
+var px = canvas.width/2;
+var py = canvas.height - 50;
+
+var wx = [100];
 var wy = [0];
-function drawBall(x,y){
+function drawBall(x,y,r){
     ctx.beginPath();{
-        ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+        ctx.arc(x, y, r, 0, Math.PI * 2, false);
         ctx.fillStyle = "green";
         ctx.fill();
     }ctx.closePath();
@@ -22,6 +26,22 @@ function drawWall(x,y){
         ctx.fillStyle = "#44DDAA";
         ctx.fill();
     }ctx.closePath();
+}
+
+function coollisionArcRect(bx,by,br,wx,wy,ww,wh) {
+
+    if( Math.abs(bx - (wx + ww/2)) <= br/2 + ww/2 && Math.abs(by - (wy + wh/2)) <= br/2 + wh/2 ){
+        if( Math.abs(bx - (wx + ww/2)) < br/2 + ww/2 ){
+            dx *= -1;
+        }else if( Math.abs(by - (wy + wh/2)) < br/2 + wh/2 ){
+            dy *= -1;
+        }else{
+            dx *= -1;
+            dy *= -1;
+        }
+
+    }
+
 }
 
 function draw(){
@@ -37,8 +57,12 @@ function draw(){
         dy *= -1;
     }
 
-    drawBall(bx,by);
+    coollisionArcRect(bx,by,br,wx[0],wy[0],40,20);
+    coollisionArcRect(bx,by,br,px,py,40,20);
+
+    drawBall(bx,by,br);
     drawWall(wx[0],wy[0]);
+    drawWall(px,py);
     
     
 }
